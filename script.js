659fa08d017b914b2c6e7075f81db8301c08484d
Sandy Raveloarisoa <sandy.rav@onja.org>
2020-08-01 09:58:30 +0300

    showed the modal

--- script.js
+++ script.js
@@ -43,17 +43,18 @@ const recipes = [{
             'Wait, put them out',
             'Add some salt on it',
         ],
-        id: 1596168522409,
+        id: 1576168552480,
     },
 ];
 
 const renderCard = () => {
     // check the recipes collection
     recipes.forEach(recipe => {
+        // generate the HTML
         const myHTML =
             `
 					<section class="section">
-					<div>
+					<div class="wrapper">
 						<h1>${recipe.title}</h1>
 						<ul class="container>
 						<li>
@@ -62,26 +63,54 @@ const renderCard = () => {
 						</p>
 						</li>
 						<li>
-							<p class="timing">It's cooked during${recipe.timing}min</p>
+							<p class="timing">It's cooked during ${recipe.timing}min</p>
 						</li>
 						<li>
-							<p class="difficulty">Difficulty:${recipe.difficulty}</p>
+							<p class="difficulty">Difficulty: ${recipe.difficulty}</p>
 						</li>
 						</ul>
-						<button class="button">
+						<button class="button-info">
 							More Information
 						</button>
 					</div>
 					</section>
 					`;
         console.log(myHTML);
-        // check the recipes collection
-        // generate the HTML
         // put it in the DOM
-        const grabBody = document.querySelector('body');
-        grabBody.insertAdjacentHTML('beforeend', myHTML);
+        const grabConatiner = document.querySelector('.container');
+        grabConatiner.insertAdjacentHTML('beforeend', myHTML);
     });
 }
 
 const generateButton = document.querySelector('button.generate');
 generateButton.addEventListener('click', renderCard);
+
+// 
+const btns = document.querySelector('.button-info');
+const outerModal = document.querySelector('.outer-modal');
+const innerModal = document.querySelector('.inner-modal');
+
+// show modal
+const openModal = () => {
+    outerModal.classList.add('open');
+}
+
+// handle escape
+const handleEscapeKey = event => {
+    if (event.key === 'Escape') {
+        outerModal.classList.remove('open');
+    }
+};
+
+// listen for the escape key
+window.addEventListener('keydown', handleEscapeKey);
+
+// handle button info click
+window.addEventListener('click', event => {
+    if (event.target.matches('.button-info')) {
+        const parents = event.target.closest('section');
+        const id = Number(parents.dataset.id);
+        const recipe = recipes.find(singleRecipe => singleRecipe.id === id);
+        openModal(recipe);
+    }
+});

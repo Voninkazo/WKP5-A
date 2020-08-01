f4ce04bbdabfbf6a3481fcf7cf38bfe04a82c104
Sandy Raveloarisoa <sandy.rav@onja.org>
2020-08-01 13:35:01 +0300

    worked on the info button

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ console.log('HELLO');
 const recipes = [{
         title: 'Eggs',
         picture: 'https://bit.ly/2ZXyiKI',
-        author: 'Loïc',
+        author: 'Harisoa',
         difficulty: 'easy',
         timing: '15',
         ingredients: ['eggs', 'salt', 'water'],
@@ -16,9 +16,9 @@ const recipes = [{
         id: 1596168482053,
     },
     {
-        title: 'Eggs',
+        title: 'Meat',
         picture: 'https://bit.ly/2ZXyiKI',
-        author: 'Loïc',
+        author: 'Sandy',
         difficulty: 'easy',
         timing: '15',
         ingredients: ['eggs', 'salt', 'water'],
@@ -31,13 +31,17 @@ const recipes = [{
         id: 1596168522409,
     },
     {
-        title: 'My recipe',
+        title: 'Chicken',
         picture: 'https://bit.ly/2ZXyiKI',
-        author: 'Loïc',
+        author: 'Alice',
         difficulty: 'easy',
         timing: '15',
         ingredients: ['eggs', 'salt', 'water'],
         steps: [
+            'Put a pan on the fire',
+            'Crack the eggs on it',
+            'Wait, put them out',
+            'Add some salt on it',
             'Put a pan on the fire',
             'Crack the eggs on it',
             'Wait, put them out',
@@ -53,26 +57,26 @@ const renderCard = () => {
         // generate the HTML
         const myHTML =
             `
-					<section class="section">
-					<div class="wrapper">
-						<h1>${recipe.title}</h1>
-						<ul class="container>
-						<li>
-						<p class="recipe-photo">
-							<img src="${recipe.picture}" alt="Delicious recipe">
-						</p>
-						</li>
-						<li>
-							<p class="timing">It's cooked during ${recipe.timing}min</p>
-						</li>
-						<li>
-							<p class="difficulty">Difficulty: ${recipe.difficulty}</p>
-						</li>
-						</ul>
-						<button class="button-info">
-							More Information
-						</button>
-					</div>
+					<section class="section" data-id=  "${recipe.id}">
+					  <div class="wrapper">
+						  <h1>${recipe.title}</h1>
+						  <ul class="container>
+						    <li>
+						      <p class="recipe-photo">
+							      <img src="${recipe.picture}" alt="Delicious recipe">
+						      </p>
+						    </li>
+						    <li>
+							    <p class="timing">It's cooked during ${recipe.timing}min</p>
+						    </li>
+						    <li>
+							  <p class="difficulty">Difficulty: ${recipe.difficulty}</p>
+						    </li>
+						    </ul>
+						    <button class="button-info">
+							    More Information
+						    </button>
+					  </div>
 					</section>
 					`;
         console.log(myHTML);
@@ -85,7 +89,7 @@ const renderCard = () => {
 const generateButton = document.querySelector('button.generate');
 generateButton.addEventListener('click', renderCard);
 
-// 
+//  grab all modals
 const btns = document.querySelector('.button-info');
 const outerModal = document.querySelector('.outer-modal');
 const innerModal = document.querySelector('.inner-modal');
@@ -108,9 +112,36 @@ window.addEventListener('keydown', handleEscapeKey);
 // handle button info click
 window.addEventListener('click', event => {
     if (event.target.matches('.button-info')) {
-        const parents = event.target.closest('section');
+        const parents = event.target.closest('.section');
         const id = Number(parents.dataset.id);
+        console.log('id', id);
+        console.log('recipes', recipes);
         const recipe = recipes.find(singleRecipe => singleRecipe.id === id);
-        openModal(recipe);
+        console.log('recipe', recipe);
+        openModal();
+        // create the outer modal that containes the recipe details
+        const myRecipe = `
+          <section>
+              <h2> ${recipe.title} by <small> ${recipe.author} </small></h2>
+              <p class="recipe-photo">
+                  <img src="${recipe.picture}" alt="Delicious recipe">
+              </p>
+              <p class="timing">It's cooked during ${recipe.timing}min</p>
+              <p class="difficulty">Difficulty: ${recipe.difficulty}</p>
+              <ul>
+                <p>
+                Follow these following steps
+                </p>
+                <li> ${recipe.steps}</li>
+                <p>
+                And these are the ingredients
+                </p>
+                <li> ${recipe.ingredients}</li>
+            </ul>
+        </section>
+        `;
+        // put it in the DOM
+        const grabModal = document.querySelector('.inner-modal');
+        grabModal.insertAdjacentHTML('beforeend', myRecipe);
     }
 });
